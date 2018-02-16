6f51aeaa864b168900c07b84c9fa526ba0506c3f
RTG <rtg@RTGs-MacBook-Pro.local>
2018-02-16 11:36:17 -0800

    Got results to display and toggle visibility with form so that user can get results without redirect needed.

--- static/js/weather.jsx
+++ static/js/weather.jsx
@@ -1,8 +1,14 @@
 
-class GetWeatherButton extends React.Component {
+class GetWeatherForm extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {value:''};
+      this.state = {
+        value:'',
+        locDescription:'',
+        locTemp:'',
+        locDay:false,
+        redirect:false
+      };
       this.handleChange = this.handleChange.bind(this);
       this.getWeather = this.getWeather.bind(this);
     }
@@ -14,33 +20,66 @@ class GetWeatherButton extends React.Component {
 
     getWeather(event) {
       event.preventDefault();
+      var respData;
     
       $.ajax({
           type: 'post',
           url: "/get-weather.json",
           data: {'location':this.state.value},
           async: false,
-          success: function (response) {
-              console.log(response);
+          success: function (resp) {
+            console.log(resp);
+            
           }
       }).done(function (data) {
-          console.log(data);
+          respData = data;
+          // if (data.locDay) {
+          //   alert("Today will be " + data.locDescription.toLowerCase() + " and " + data.locTemp + "°F");
+          // } else {
+          //   alert("This evening will be " + data.locDescription.toLowerCase() + " and " + data.locTemp + "°F");
+          // }
       });
+
+      this.setState({
+        locDescription: respData.description,
+        locTemp: respData.temp,
+        locDay: respData.is_day,
+        redirect:true
+      });
+
+      setTimeout(() => {
+        this.showResults();
+      }, 1000);
+    }
+
+    showResults() {
+      if (this.state.locDay) {
+        $('#resultsDiv').append("Today will be " + this.state.locDescription.toLowerCase() + " and " + this.state.locTemp + "°F");
+      } else {
+        $('#resultsDiv').append("This evening will be " + this.state.locDescription.toLowerCase() + " and " + this.state.locTemp + "°F");
+      }
+      $('#weatherForm').toggle();
+      $('#resultsDiv').toggle();
     }
 
 
     render() {
       return  (
-              <form onSubmit={this.getWeather}>
-                <input type="text" value={this.state.value} onChange={this.handleChange} name="location-name" />
-                <input type="submit" value="Tell me now!" />
-              </form>
+        <div>
+          <div id="weatherForm">
+            <form onSubmit={this.getWeather}>
+              <input type="text" value={this.state.value} onChange={this.handleChange} name="location-name" />
+              <input type="submit" value="Tell me now!" />
+            </form>
+          </div>
+          <div id="resultsDiv" hidden></div>
+        </div>
       );
     }
 }
 
 
 ReactDOM.render(
-    <GetWeatherButton />,
+    <GetWeatherForm />,
     document.getElementById('root'),
 );
